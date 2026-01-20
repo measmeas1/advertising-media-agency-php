bf6e8c5ebec4aaf19e55e6487c6ad4435de7c804
TouPhanneat <zeiineat1551@icloud.com>
2026-01-20 14:13:40 +0700

    Edit File Product

--- src/admin/products/create.php
+++ src/admin/products/create.php
@@ -30,7 +30,9 @@ if(isset($_POST['save'])) {
     <input type="text" name="category_id" placeholder="Category ID" class="p-2 border rounded w-full mb-3" required>
     <input type="text" name="title" placeholder="Title" class="p-2 border rounded w-full mb-3" required>
     <input type="number" name="price" placeholder="Price" class="p-2 border rounded w-full mb-3" required>
-    <input type="number" name="description" placeholder="Description" class="p-2 border rounded w-full mb-3" required>
+     <textarea name="description"
+              placeholder="Description"
+              class="p-2 border rounded w-full mb-3" required></textarea>
     
     <div class="flex space-x-2">
         <button type="submit" name="save" class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">Save</button>
