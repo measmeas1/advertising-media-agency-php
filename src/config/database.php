967503d7daff8a3cc6d4e515890aeea541d67ecb
TouPhanneat <zeiineat1551@icloud.com>
2026-01-20 20:32:05 +0700

    Update env

--- src/config/database.php
+++ src/config/database.php
@@ -14,7 +14,7 @@ function env($key, $default = null)
 return [
     'host' => env('DB_HOST', '127.0.0.1'),
     'port' => env('DB_PORT', '3306'),
-    'dbname' => env('DB_NAME'),
-    'user' => env('DB_USER'),
-    'pass' => env('DB_PASS'),
+    'dbname' => env('DB_NAME','ad_agency_db'),
+    'user' => env('DB_USER','root'),
+    'pass' => env('DB_PASS','    '),
 ];
